894f401ec4a79c7bea194a989f7c531fab208a94
1jinie <wkddnjswlsn@naver.com>
2026-05-06 14:43:39 +0900

    feat : 사원정보 추가 기능 완료

--- src/main/java/controller/AppendController.java
+++ src/main/java/controller/AppendController.java
@@ -1,5 +1,9 @@
 package controller;
 
+import exception.EmployeeException;
+import service.EmployeeService;
+import vo.EmployeeVO;
+
 import java.util.Scanner;
 
 public class AppendController implements Controller {
@@ -10,5 +14,24 @@ public class AppendController implements Controller {
         String id = sc.nextLine();
 
         // 중복 체크
+        try {
+            EmployeeService.getInstance().checkDuplicateEmployeeID(id);
+
+            System.out.println("등록할 사원명: ");
+            String name = sc.nextLine();
+            System.out.println("등록할 직급: ");
+            String position = sc.nextLine();
+            System.out.println("등록할 급여: ");
+            int salary = sc.nextInt(); sc.nextLine();
+            System.out.println("등록할 입사일: ");
+            String hireDate = sc.nextLine();
+
+            boolean flag = EmployeeService.getInstance().appendEmployee(new EmployeeVO(id, name, position, salary, hireDate));
+
+            System.out.println(flag ? "사원정보 등록 성공" : "등록 실패");
+
+        } catch (EmployeeException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
